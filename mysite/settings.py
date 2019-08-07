22b7a8b4f991070f32e623436ec7c560067c362d
Saloni Soniminde <salonisoniminde@gmail.com>
2019-08-07 19:47:17 +0530

    voting in django

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,13 +23,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 # Application definition
 INSTALLED_APPS = [
+    'polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
+
 ]
 
 MIDDLEWARE = [
